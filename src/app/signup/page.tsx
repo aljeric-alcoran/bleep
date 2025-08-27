0a0eba8da60bc0ded942badbd2145867714a9f27
AL <alcoran026@gmail.com>
2025-08-28 01:39:50 +0800

    [auth] Added verify otp and signup component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,12 +3,9 @@
 import { z } from "zod"
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Label } from "@/components/ui/label";
 import {
    Card,
    CardAction,
@@ -26,18 +23,17 @@ import {
    FormLabel,
    FormMessage,
  } from "@/components/ui/form"
+import Link from "next/link";
 
 const formSchema = z.object({
    email: z.email(),
-   password: z.string().min(8).max(50),
 })
 
-export default function LoginForm() {
+export default function SignupForm() {
    const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
       defaultValues: {
          email: "",
-         password: "",
       },
    })
 
@@ -49,8 +45,8 @@ export default function LoginForm() {
       <div className="flex justify-center items-center h-dvh">
          <Card className="w-full max-w-sm">
             <CardHeader>
-               <CardTitle className="text-lg font-semibold">Welcome Back!</CardTitle>
-               <CardDescription>Login to your existing bleep account</CardDescription>
+               <CardTitle className="text-lg font-semibold">Welcome to Bleep</CardTitle>
+               <CardDescription>Register a new account</CardDescription>
                <CardAction>
                   <Link href="/">
                      <Button className="">
@@ -80,36 +76,14 @@ export default function LoginForm() {
                               )}
                            />
                         </div>
-                        <div className="text-gray-900 dark:text-white">
-                           <FormField
-                              control={form.control}
-                              name="password"
-                              render={({ field }) => (
-                                 <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                       <Input type="password" placeholder="" {...field} />
-                                    </FormControl>
-                                    <FormMessage className="text-xs" />
-                                 </FormItem>
-                              )}
-                           />
-                        </div>
                      </div>
-                     <div className="flex justify-between items-center">
-                        <div className="flex items-center gap-2">
-                           <Checkbox id="terms" />
-                           <Label htmlFor="terms" className="text-xs">Remember me</Label>
-                        </div>
-                        <Link href="/forgot-password" className="text-xs text-gray-400 dark:text-white">Forgot Password</Link>
-                     </div>
-                     <Button type="submit" className="w-full">Submit</Button>
+                     <Button type="submit" className="w-full">Send Verification Code</Button>
                   </form>
                </Form>
             </CardContent>
             <CardFooter className="flex flex-col gap-4">
-               <p className="text-xs text-gray-400 dark:text-white">Or sign in using</p>
-               <Button type="submit" className="w-full bg-gray-50 text-gray-900 flex items-center justify-center hover:bg-gray-100">
+               <p className="text-xs text-gray-400 dark:text-white">Or sign up using</p>
+               <Button type="submit" className="w-full bg-gray-200 text-gray-900 flex items-center justify-center hover:bg-gray-300">
                   <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                      <path fillRule="evenodd" d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z" clipRule="evenodd"/>
                   </svg>
