eee57fc170785966776df37cff36815e942817c5
AL <alcoran026@gmail.com>
2025-08-30 00:45:08 +0800

    [auth] Refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,36 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(req: NextRequest) {
    const token = req.cookies.get("accessToken")?.value;
+   const { pathname } = req.nextUrl;
+   
+   let isTokenValid = false;
 
-   if (!token) {
-      return NextResponse.redirect(new URL("/login", req.url));
+   if (token) {
+      try {
+         const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+         await jwtVerify(token, secret);
+         isTokenValid = true;
+      } catch (err) {
+         console.log("JWT Verification Error:", err);
+      }
    }
 
-   try {
-      const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
-      await jwtVerify(token, secret);
-
-      return NextResponse.next();
-   } catch (err) {
-      console.log("JWT Verification Error:", err);
-      return NextResponse.redirect(new URL("/login", req.url));
+   if (isTokenValid) {
+      if (pathname.startsWith("/login")) {
+         return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+   } else {
+      if (pathname.startsWith("/dashboard")) {
+         return NextResponse.redirect(new URL("/login", req.url));
+      }
    }
+
+   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard", "/dashboard/:path*"],
+   matcher: ["/login", "/dashboard", "/dashboard/:path*"],
 };
 
