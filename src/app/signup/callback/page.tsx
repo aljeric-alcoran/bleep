cce377a9672cfdb0a8d7565a1cd4df4da8808236
AL <alcoran026@gmail.com>
2025-09-02 20:13:39 +0800

    [google-auth] Added design and refactor some code.

--- src/app/signup/callback/page.tsx
+++ src/app/signup/callback/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { redirectUser } from "@/lib/api/auth";
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Callback() {
    const router = useRouter();
@@ -11,20 +13,24 @@ export default function Callback() {
       const checkAuth = async () => {
          try {
             const response = await redirectUser();
-            if (response.accessToken) {
-               setTimeout(() => {
-                  router.replace("/dashboard");
-               }, 1000)
-            } else {
-               router.replace("/login?error=auth_failed");
-            }
+            
+            if (response.accessToken) router.replace("/dashboard");
+            else router.replace("/login?error=auth_failed");
          } catch (err) {
             router.replace("/login?error=network_error");
          }
       };
-
       checkAuth();
    }, [router]);
 
-   return <p>Checking login...</p>;
+   return (
+      <div className="flex justify-center items-center h-dvh">
+         <Card className="w-full max-w-sm h-80 flex flex-col items-center justify-center">
+            <CardContent className="flex flex-col items-center gap-4">
+               <Image className="animate-bounce" src="/logo.png" width={40} height={40} alt="Bleep logo" />
+               <p>Redirecting user...</p>
+            </CardContent>
+         </Card>
+      </div>
+   );
 }
