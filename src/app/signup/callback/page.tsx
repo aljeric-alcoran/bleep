5915236e36dfc388b96a38febebad4d6de6e32b2
AL <alcoran026@gmail.com>
2025-09-02 19:56:24 +0800

    [google-auth] Add callback page, loads data after user logged in or signup using google

--- src/app/signup/callback/page.tsx
+++ src/app/signup/callback/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { redirectUser } from "@/lib/api/auth";
+
 export default function Callback() {
-   return (
-      <div>Loading component when signup using google.</div>
-   )
+   const router = useRouter();
+
+   useEffect(() => {
+      const checkAuth = async () => {
+         try {
+            const response = await redirectUser();
+            if (response.accessToken) {
+               setTimeout(() => {
+                  router.replace("/dashboard");
+               }, 1000)
+            } else {
+               router.replace("/login?error=auth_failed");
+            }
+         } catch (err) {
+            router.replace("/login?error=network_error");
+         }
+      };
+
+      checkAuth();
+   }, [router]);
+
+   return <p>Checking login...</p>;
 }
