18e0b79fb3e3071a47bb5d47ffe74622b960ba28
AL <alcoran026@gmail.com>
2025-09-02 23:31:07 +0800

    [google-auth] Remove rewrite origin

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
    /* config options here */
    devIndicators: false,
-   async rewrites() {
-      return [
-         {
-            source: "/api/:path*",
-            destination: "http://localhost:3000/api/:path*",
-         },
-      ];
-   },
 };
 
 export default nextConfig;
