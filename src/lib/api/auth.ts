d14fe877e2fcecd56451014de76b519a8714a300
AL <alcoran026@gmail.com>
2025-09-04 11:31:07 +0800

    [google-auth] Refactor: Implement API call directly from the server.

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,25 +1,11 @@
-'use server';
-
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-// export async function loginUser({ email, password }: { email: string, password: string }) {
-//    const response = await fetch(`http://localhost:3002/api/auth/login`, {
-//       method: 'POST',
-//       headers: {
-//          'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ email, password }),
-//    });
- 
-//    const data = await response.json();
-//    return data; 
-//  }
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export async function loginUser({ email, password }: { email: string, password: string }) {
-   const response = await fetch(`http://localhost:3000/api/auth/login`, {
+   const response = await fetch(`${baseURL}/api/auth/login`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+         'Content-Type': 'application/json',
+      },
       body: JSON.stringify({ email, password }),
       credentials: 'include', 
    });
@@ -28,59 +14,35 @@ export async function loginUser({ email, password }: { email: string, password: 
 }
 
 export async function googleLogin() {
-   const response = await fetch('/api/auth/google', {
-      method: 'GET',
-      headers: {
-         'Content-Type': 'application/json'
-      }
-   });
+   const response = await fetch('/api/auth/google');
    return response.json();
 }
 
 export async function logoutUser() {
    const response = await fetch('/api/auth/logout', {
       method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-      },
    });
-   
-   if (response.ok) {
-      window.location.href = '/login'; 
-   } else {
-      console.error('Logout failed.');
-   }
+
+   if (response.ok) window.location.href = '/login'; 
+   else console.error('Logout failed.');
 }
 
 export async function redirectUser() {
-   const response = await fetch('/api/auth/me', {
+   const response = await fetch(`${baseURL}/api/auth/me`, {
       method: 'GET',
       credentials: 'include',
    });
-   return response.json();
+   return await response.json();
 }
 
-export async function requestAccessToken() {
-   const cookieStore = await cookies();
-   const refreshToken = cookieStore.get('refreshToken')?.value;
- 
-   if (!refreshToken) {
-      return NextResponse.json({ message: 'No refresh token found' }, { status: 401 });
-   }
- 
-   const response = await fetch(`http://localhost:3002/api/auth/refresh`, {
-      method: 'POST',
+export async function requestAccessToken(refreshToken: string) {
+   const response = await fetch(`${baseURL}/api/auth/refresh`, {
+      method: 'GET',
       headers: {
          'Content-Type': 'application/json',
+         'Authorization': `Bearer ${refreshToken}`
       },
-      body: JSON.stringify({ refreshToken }),
    });
  
-   const data = await response.json();
- 
-   if (!data.accessToken) {
-      return NextResponse.json({ message: 'Failed to refresh token' }, { status: response.status });
-   }
- 
-   return NextResponse.json(data, { status: 200 });
+   return await response.json();
 }
