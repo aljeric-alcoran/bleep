0d0ce0b7ef68d9aaed959a60be4959e2ebdd5fa1
AL <alcoran026@gmail.com>
2025-09-12 11:53:03 +0800

    [refactor login] Setup rubik fonts

---
+++ src/app/fonts.tsx
@@ -0,0 +1,76 @@
+import localFont from 'next/font/local'
+
+export const rubik = localFont({
+   src: [
+      {
+         path: '../../public/fonts/Rubik-Light.ttf',
+         weight: '300',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-LightItalic.ttf',
+         weight: '300',
+         style: 'italic',
+      },
+      {
+         path: '../../public/fonts/Rubik-Regular.ttf',
+         weight: '400',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-Italic.ttf',
+         weight: '400',
+         style: 'italic',
+      },
+      {
+         path: '../../public/fonts/Rubik-Medium.ttf',
+         weight: '500',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-MediumItalic.ttf',
+         weight: '500',
+         style: 'italic',
+      },
+      {
+         path: '../../public/fonts/Rubik-SemiBold.ttf',
+         weight: '600',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-SemiBoldItalic.ttf',
+         weight: '600',
+         style: 'italic',
+      },
+      {
+         path: '../../public/fonts/Rubik-Bold.ttf',
+         weight: '700',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-BoldItalic.ttf',
+         weight: '700',
+         style: 'italic',
+      },
+      {
+         path: '../../public/fonts/Rubik-ExtraBold.ttf',
+         weight: '800',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-ExtraBoldItalic.ttf',
+         weight: '800',
+         style: 'italic',
+      },
+      {
+         path: '../../public/fonts/Rubik-Black.ttf',
+         weight: '900',
+         style: 'normal',
+      },
+      {
+         path: '../../public/fonts/Rubik-BlackItalic.ttf',
+         weight: '900',
+         style: 'italic',
+      },
+   ],
+})
