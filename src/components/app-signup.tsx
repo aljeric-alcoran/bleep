8edce8b3301e0a2ff73fa5cad31538a7ba178e9a
AL <alcoran026@gmail.com>
2025-09-12 15:19:42 +0800

    [refactor-signup] Add conditions for step during signup process

--- src/components/app-signup.tsx
+++ src/components/app-signup.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useSignup } from "@/app/context/SignupContext";
 import GoogleSignup from "@/components/google";
 import SignupForm from "./forms/signup-form";
 import Image from "next/image";
@@ -10,14 +13,36 @@ import {
    CardHeader,
    CardTitle,
 } from "@/components/ui/card"
+import { useEffect, useState } from "react";
+import VerifyOTPForm from "./forms/verify-otp-form";
+import { title } from "process";
 
 export default function Signup() {
+   const { email, step, setStep, setOtp } = useSignup();
+   const cardDescriptions = [
+      {
+         title: "Welcome to Bleep",
+         description: "Register a new account",
+      },
+      {
+         title: "One-Time Password",
+         description: "Please enter the one-time password sent to your email.",
+      },
+      {
+         title: "Create your account",
+         description: "Please fill out the following information to register.",
+      }
+   ]
+
+   useEffect(() => {
+      setStep(step);
+   }, [email]);
    return (
       <div className="w-full flex justify-center items-center">
          <Card className="w-full shadow-none border-transparent p-0">
             <CardHeader className="p-0">
-               <CardTitle className="text-lg font-semibold">Welcome to Bleep</CardTitle>
-               <CardDescription>Register a new account</CardDescription>
+               <CardTitle className="text-lg font-semibold">{cardDescriptions[step - 1].title}</CardTitle>
+               <CardDescription>{cardDescriptions[step - 1].description}</CardDescription>
                <CardAction>
                   <Image
                      src="/logo.png"
@@ -29,12 +54,15 @@ export default function Signup() {
                </CardAction>
             </CardHeader>
             <CardContent className="p-0 pt-4">
-               <SignupForm />
+               { step === 1 && <SignupForm /> }
+               { step === 2 && <VerifyOTPForm setOtp={setOtp} email={email}/> }
             </CardContent>
-            <CardFooter className="flex flex-col gap-4 p-0">
-               <p className="text-xs text-gray-400 dark:text-white">Or sign in using</p>
-               <GoogleSignup />
-            </CardFooter>
+            { step === 1 && (
+               <CardFooter className="flex flex-col gap-4 p-0">
+                  <p className="text-xs text-gray-400 dark:text-white">Or sign in using</p>
+                  <GoogleSignup />
+               </CardFooter>
+            )}
          </Card>
       </div>
    );
