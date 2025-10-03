8a5a3d61dddf49174466e0789ea5ccb50e4f5f41
AL <alcoran026@gmail.com>
2025-10-03 21:13:53 +0800

    [dashboard-admin] Revert changes from layout; Render only children, remove other html tags

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,4 @@
-import { SignupProvider } from "@/app/context/SignupContext";
 import type { Metadata } from "next";
-import { Toaster } from "@/components/ui/sonner"
-import AppHeader from "@/components/app-header";
-import AppSearch from "@/components/app-search";
 import "@/app/assets/globals.css";
 
 export const metadata: Metadata = {
@@ -16,18 +12,7 @@ export default function RootLayout({
 }>) {
    return (
       <>
-         <Toaster position="top-right" richColors/>
-         <SignupProvider>
-            <header className="border-b pb-6 bg-linear-to-t from-red-600 to-red-800 text-white">
-               <nav className="bg-transparent border-gray-200 px-4 lg:px-6 mx-auto max-w-screen-xl">
-                  <div className="w-full flex flex-wrap justify-between items-center">
-                     <AppHeader />
-                     <AppSearch />
-                  </div>
-               </nav>
-            </header>
-            {children}
-         </SignupProvider>
+         {children}
       </>
    );
 }
