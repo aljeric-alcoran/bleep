e6597724e862960ca648ecff8e04f7096adf32cf
AL <alcoran026@gmail.com>
2025-10-08 16:19:34 +0800

    [categories] Render categories table

--- src/app/(account)/categories/page.tsx
+++ src/app/(account)/categories/page.tsx
@@ -1,8 +1,29 @@
-export default function Categories() {
-   
+import { columns, Category } from '@/components/tables/categories/columns'
+import { DataTable } from '@/components/tables/data-table';
+
+async function getData(): Promise<Category[]> {
+   return [
+      {
+         id: "728ed52f",
+         name: "Electronics",
+         order: 1,
+         isActive: "Active",
+      }
+   ]
+}
+
+export default async function Categories() {
+   const data = await getData()
+
    return (
-      <div className="flex">
-         <h1>Categories</h1>
-      </div>
+      <>
+         <div className="flex">
+            <h1 className="text-lg font-semibold">Categories</h1>
+         </div>
+
+         <div className="container mx-auto pt-5 pb-10">
+            <DataTable columns={columns} data={data} />
+         </div>
+      </>
    );
 }
