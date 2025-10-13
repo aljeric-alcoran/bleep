0f000770916f497f6c94e2abf54c857eea2f8aa2
AL <alcoran026@gmail.com>
2025-10-13 23:12:43 +0800

    [categories] Integrate GET category api

--- src/app/(account)/categories/page.tsx
+++ src/app/(account)/categories/page.tsx
@@ -1,28 +1,41 @@
-import { columns, Category } from '@/components/tables/categories/columns'
+"use client"
+
+import { columns } from '@/components/tables/categories/columns'
 import { DataTable } from '@/components/tables/data-table';
+import { AddCategoryForm } from '@/components/forms/add-category-form';
+import { LayoutList } from 'lucide-react';
+import { allCategories } from '@/lib/api/categories';
+import { useEffect, useState } from 'react';
 
-async function getData(): Promise<Category[]> {
-   return [
-      {
-         id: "728ed52f",
-         name: "Electronics",
-         order: 1,
-         isActive: "Active",
+export default function Categories() {
+   const [categories, setCategories] = useState([])
+
+
+   useEffect(() => {
+      async function fetchCategories() {
+        const { data } = await allCategories();
+        setCategories(data)
+        console.log(data);
       }
-   ]
-}
-
-export default async function Categories() {
-   const data = await getData()
+  
+      fetchCategories();
+    }, []);
 
    return (
       <>
-         <div className="flex">
+         <div className="flex items-center gap-2">
+            <LayoutList className="w-5"/>
             <h1 className="text-lg font-semibold">Categories</h1>
          </div>
 
          <div className="container mx-auto pt-5 pb-10">
-            <DataTable columns={columns} data={data} />
+            <DataTable 
+               columns={columns} 
+               data={categories} 
+               addActionSlot={
+                  <AddCategoryForm/>
+               }
+            />
          </div>
       </>
    );
