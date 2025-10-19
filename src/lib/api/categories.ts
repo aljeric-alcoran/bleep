e013ab3eb101b691c53080eb4b29dc57f18ddf80
AL <alcoran026@gmail.com>
2025-10-19 21:11:02 +0800

    [categories] add API call function for adding category

--- src/lib/api/categories.ts
+++ src/lib/api/categories.ts
@@ -1,3 +1,5 @@
+import { Category } from "../types/category-type";
+
 const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export async function fetchCategories() {
@@ -11,4 +13,19 @@ export async function fetchCategories() {
 
    const data = await response.json();
    return { status: response.status, message: data.message, data: data.data };
+}
+
+export async function addCategory(categoryObject: Category) {
+   console.log(categoryObject);
+   const response = await fetch(`${baseURL}/categories`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      credentials: 'include', 
+      body: JSON.stringify(categoryObject),
+   });
+
+   const data = await response.json();
+   return data;
 }
