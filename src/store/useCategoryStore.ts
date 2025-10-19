8255d4975c4392f17481410270251fd0d3be6488
AL <alcoran026@gmail.com>
2025-10-19 23:06:13 +0800

    [categories] Add category store function for adding item in categories array

--- src/store/useCategoryStore.ts
+++ src/store/useCategoryStore.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
 import { Category } from "@/lib/types/category-type";
-const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 type Categories = {
    categories: Category[];
    setCategories: (categories: Category[]) => void;
+   addToCategoryStore: (category: Category) => void;
 }
 
 export const useCategoryStore = create<Categories>((set) => ({
    categories: [],
-   setCategories: (items) => set({ categories: items })
+   setCategories: (items) => set({ categories: items }),
+   addToCategoryStore: (category) => set((state) => ({
+      categories: [...state.categories, category],
+   })),
 }));
