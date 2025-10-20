edaab9ebc3b9ab25836fd0fd93405d95099a5530
AL <alcoran026@gmail.com>
2025-10-20 15:57:29 +0800

    [categories] Add methods for updating and deleting items in the store; Rename methods

--- src/store/useCategoryStore.ts
+++ src/store/useCategoryStore.ts
@@ -4,13 +4,25 @@ import { Category } from "@/lib/types/category-type";
 type Categories = {
    categories: Category[];
    setCategories: (categories: Category[]) => void;
-   addToCategoryStore: (category: Category) => void;
+   addCategoryFromStore: (category: Category) => void;
+   updateCategoryFromStore: (updatedCategory: Category) => void;
+   removeCategoryFromStore: (categoryId: string | undefined) => void;
 }
 
 export const useCategoryStore = create<Categories>((set) => ({
    categories: [],
    setCategories: (items) => set({ categories: items }),
-   addToCategoryStore: (category) => set((state) => ({
+   addCategoryFromStore: (category) => set((state) => ({
       categories: [...state.categories, category],
    })),
+   updateCategoryFromStore: (updatedCategory) =>
+      set((state) => ({
+         categories: state.categories.map((category) =>
+            category._id === updatedCategory._id ? updatedCategory : category
+         ),
+      })),
+   removeCategoryFromStore: (categoryId) =>
+      set((state) => ({
+        categories: state.categories.filter((category) => category._id !== categoryId),
+      })),
 }));
