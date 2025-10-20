6aed2bc08723f7726ab2bb9e05e93cf78e6c1f44
AL <alcoran026@gmail.com>
2025-10-20 13:54:52 +0800

    [categories] Add category component

---
+++ src/app/(account)/categories/add-category.tsx
@@ -0,0 +1,65 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import {
+   Dialog,
+   DialogContent,
+   DialogDescription,
+   DialogHeader,
+   DialogTitle,
+   DialogTrigger,
+} from "@/components/ui/dialog";
+import { addNewCategory } from "@/lib/api/categories";
+import { useCategoryStore } from '@/store/useCategoryStore';
+import { useState } from "react";
+import { CategoryForm } from "@/components/forms/category-form";
+import { CategoryFormSchema, useCategoryForm } from "@/schema/category.schema";
+
+export function AddCategory() {
+   const addToCategoryStore = useCategoryStore((state) => state.addToCategoryStore);
+   const [open, setOpen] = useState(false);
+   const [error, setError] = useState<string | null>(null);
+   const form = useCategoryForm();
+
+   async function onSubmit(values: CategoryFormSchema): Promise<void> {
+      try {
+         const { metadata, parent, ...data } = values;
+         const payload = {
+            ...data,
+            parent: parent === '' ? null : parent,
+            metadata: {
+               keywords: metadata?.keywords?.split(" "),
+               seoTitle: metadata.seoTitle,
+               seoDescription: metadata.seoDescription
+            }
+         }
+         const response = await addNewCategory(payload);
+         addToCategoryStore(response.data);
+         form.reset();
+         setOpen(false);
+      } catch(error: any) {
+         setError(error.message)
+         console.log(error);
+      }
+   }
+
+   return (
+      <Dialog open={open} onOpenChange={setOpen}>
+         <DialogTrigger asChild>
+            <Button>Add Category</Button>
+         </DialogTrigger>
+         <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+               <DialogTitle>Add New Category</DialogTitle>
+               <DialogDescription>
+                  Fill in the details below to create a new category.
+               </DialogDescription>
+            </DialogHeader>
+            <CategoryForm 
+               form={form} 
+               error={error} 
+               onSubmit={onSubmit}
+            />
+         </DialogContent>
+      </Dialog>
+   )
+}
