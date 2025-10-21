1b0771d2898f2acb896cfcccc324a0b2a7e95a97
AL <alcoran026@gmail.com>
2025-10-21 22:49:26 +0800

    [user-profile] Refactor user object types

--- src/lib/types/user-type.ts
+++ src/lib/types/user-type.ts
@@ -1,6 +1,8 @@
 export type User = {
+   _id?: string;
    firstname: string;
    lastname: string;
-   phoneNumber: string;
-   email: string;
+   phoneNumber?: string;
+   email?: string;
+   gender?: "male" | "female" | "other" | null;
 }
