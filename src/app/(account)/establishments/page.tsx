b952e29b29c4e89caff01f6c1956c82b98b226ab
AL <alcoran026@gmail.com>
2025-11-10 16:23:56 +0800

    [establishment] Added establishment page

---
+++ src/app/(account)/establishments/page.tsx
@@ -0,0 +1,12 @@
+import { Store } from "lucide-react";
+
+export default function Establishments() {
+   return (
+      <> 
+         <div className="flex items-center gap-2">
+            <Store className="w-5"/>
+            <h1 className="text-lg font-semibold">Establishments</h1>
+         </div>
+      </>
+   )
+}
