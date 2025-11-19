e3c1780df376c0bc5db658be00e8824de27ecb55
AL <alcoran026@gmail.com>
2025-11-19 23:21:02 +0800

    [establishment] Add route for establishment

---
+++ src/app/api/v1/establishments/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+const bleepAPIURL = process.env.NEXT_PUBLIC_BACKEND_URL
+
+export async function GET(req: NextRequest) {
+   try {
+      const cookieHeader = req.headers.get("cookie") || "";
+      console.log("Cookie: ", cookieHeader);
+      const response = await fetch(`${bleepAPIURL}/establishments`, {
+         method: "GET",
+         headers: {
+            "Content-Type": "application/json",
+            cookie: cookieHeader,
+         },
+         cache: "no-store",
+      });
+
+      if (!response.ok) {
+         return NextResponse.json(
+            { error: `Failed to fetch establishments (${response.status})` },
+            { status: response.status }
+         );
+      }
+
+      const data = await response.json();
+      return NextResponse.json(data);
+   } catch (error: any) {
+      return NextResponse.json(
+         { error: error.message || "Internal Server Error" },
+         { status: 500 }
+      );
+   }
+}
