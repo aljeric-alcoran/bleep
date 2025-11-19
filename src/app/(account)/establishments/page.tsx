5c6d9c8f7044b5ff4b3eee6b31bbff1c254ecb3a
AL <alcoran026@gmail.com>
2025-11-19 23:26:11 +0800

    [establishment] Implement API integration, loading components and displaying empty list

--- src/app/(account)/establishments/page.tsx
+++ src/app/(account)/establishments/page.tsx
@@ -1,59 +1,51 @@
+'use client'
+
+import EmptyList from "./EmptyList";
+import LoadingSkeleton from "./LoadingSkeleton";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
-import { EllipsisVertical, Plus, Store, Warehouse } from "lucide-react";
+import { fetchEstablishments } from "@/lib/api/establishment";
+import { EllipsisVertical, Store } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Establishments() {
+   const { isLoading, isError, data, error } = useQuery({ queryKey: ['establishments'], queryFn: fetchEstablishments})
+   const hasEstablishments = data?.data.length > 0;
    return (
       <> 
          <div className="flex items-center gap-2">
             <Store className="w-5"/>
             <h1 className="text-lg font-semibold">Establishments</h1>
          </div>
+         {isLoading && <LoadingSkeleton />}
 
-         <div className="mt-6">
-            <Card className="w-full max-w-sm relative">
-               <CardHeader>
-                  <CardTitle>Bleep Store</CardTitle>
-                  <Button variant="link" className="absolute top-2 right-2">
-                     <EllipsisVertical />
-                  </Button>
-               </CardHeader>
-               <CardContent className="-mt-2">
-                  <CardDescription className="space-y-3">
-                     <div>
-                        <p className="text-gray-900 text-xs">Address</p>
-                        Enter your email below to login to your account
-                     </div>
+         {!isLoading && !hasEstablishments && <EmptyList />}
 
-                     <div>
-                        <p className="text-gray-900 text-xs">Phone</p>
-                        +63 9 163 518 635
-                     </div>
-                  </CardDescription>
-               </CardContent>
-            </Card>
-         </div>
+         {!isLoading && hasEstablishments && (
+            <div className="mt-6">
+               <Card className="w-full max-w-sm relative">
+                  <CardHeader>
+                     <CardTitle>Bleep Store</CardTitle>
+                     <Button variant="link" className="absolute top-2 right-2">
+                        <EllipsisVertical />
+                     </Button>
+                  </CardHeader>
+                  <CardContent className="-mt-2">
+                     <CardDescription className="space-y-3">
+                        <div>
+                           <p className="text-gray-900 text-xs">Address</p>
+                           Enter your email below to login to your account
+                        </div>
 
-         <div className="mt-6">
-            <Empty className="border border-dashed">
-               <EmptyHeader>
-                  <EmptyMedia variant="icon">
-                     <Warehouse />
-                  </EmptyMedia>
-                  <EmptyTitle>No Establishment Yet</EmptyTitle>
-                  <EmptyDescription>
-                     Ready to start selling? Create your first establishment and showcase your products.
-                  </EmptyDescription>
-               </EmptyHeader>
-               <EmptyContent>
-                  <Button className="font-normal" variant="default" size="sm">
-                     <Plus />
-                     Create Establishment
-                  </Button>
-               </EmptyContent>
-            </Empty>
-         </div>
+                        <div>
+                           <p className="text-gray-900 text-xs">Phone</p>
+                           +63 9 163 518 635
+                        </div>
+                     </CardDescription>
+                  </CardContent>
+               </Card>
+            </div>
+         )}
       </>
    )
 }
