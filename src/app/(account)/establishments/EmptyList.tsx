d09717efea55c903e22ce2de0cbac54f5c97f322
AL <alcoran026@gmail.com>
2025-11-19 23:23:54 +0800

    [establishment] Separate empty component

---
+++ src/app/(account)/establishments/EmptyList.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+import { Plus, Warehouse } from "lucide-react";
+
+export default function EmptyList() {
+   return (
+      <div className="mt-6">
+         <Empty className="border border-dashed">
+            <EmptyHeader>
+               <EmptyMedia variant="icon">
+                  <Warehouse />
+               </EmptyMedia>
+               <EmptyTitle>No Establishment Yet</EmptyTitle>
+               <EmptyDescription>
+                  Ready to start selling? Create your first establishment and showcase your products.
+               </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+               <Button className="font-normal" variant="default" size="sm">
+                  <Plus />
+                  Create Establishment
+               </Button>
+            </EmptyContent>
+         </Empty>
+      </div>
+   );
+}
