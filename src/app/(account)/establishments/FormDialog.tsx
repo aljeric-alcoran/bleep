4ff37aed96875f78114c0e499a28c23873502f5b
AL <alcoran026@gmail.com>
2025-11-20 22:10:44 +0800

    [establishment] Establishment form is added on the dialog

--- src/app/(account)/establishments/FormDialog.tsx
+++ src/app/(account)/establishments/FormDialog.tsx
@@ -1,3 +1,4 @@
+import EstablishmentForm from "@/components/forms/EstablishmentForm";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 export default function FormDialog({
@@ -17,7 +18,7 @@ export default function FormDialog({
                      Fill in the details below to create an establishment.
                   </DialogDescription>
                </DialogHeader>
-               
+               <EstablishmentForm setOpen={setOpen}/>
             </DialogContent>
          </Dialog>
       </>
