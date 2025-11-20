685bf0d15e3c5bf760116f2236742ce30266562f
AL <alcoran026@gmail.com>
2025-11-20 22:09:30 +0800

    [establishment] POST request is added as API endpoint

--- src/app/api/v1/establishments/route.ts
+++ src/app/api/v1/establishments/route.ts
@@ -4,7 +4,7 @@ const bleepAPIURL = process.env.NEXT_PUBLIC_BACKEND_URL
 export async function GET(req: NextRequest) {
    try {
       const cookieHeader = req.headers.get("cookie") || "";
-      console.log("Cookie: ", cookieHeader);
+
       const response = await fetch(`${bleepAPIURL}/establishments`, {
          method: "GET",
          headers: {
@@ -17,6 +17,38 @@ export async function GET(req: NextRequest) {
       if (!response.ok) {
          return NextResponse.json(
             { error: `Failed to fetch establishments (${response.status})` },
+            { status: response.status }
+         );
+      }
+
+      const data = await response.json();
+      return NextResponse.json(data);
+   } catch (error: any) {
+      return NextResponse.json(
+         { error: error.message || "Internal Server Error" },
+         { status: 500 }
+      );
+   }
+}
+
+export async function POST(req: NextRequest) {
+   try {
+      const cookieHeader = req.headers.get("cookie") || "";
+      const body = await req.json();
+
+      const response = await fetch(`${bleepAPIURL}/establishments`, {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+            cookie: cookieHeader,
+         },
+         body: JSON.stringify(body),
+         cache: "no-store",
+      });
+
+      if (!response.ok) {
+         return NextResponse.json(
+            { error: `Failed to create establishment (${response.status})` },
             { status: response.status }
          );
       }
