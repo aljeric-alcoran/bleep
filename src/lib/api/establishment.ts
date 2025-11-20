3c585da863d323bf446fafbb704d469b0c387046
AL <alcoran026@gmail.com>
2025-11-20 22:11:55 +0800

    [establishment] Added API call function for adding new establishment

--- src/lib/api/establishment.ts
+++ src/lib/api/establishment.ts
@@ -1,8 +1,20 @@
+import { Establishment } from "../models";
+
 export async function fetchEstablishments() {
    const response = await fetch("/api/v1/establishments", {
       method: 'GET',
    });
    
    if (!response.ok) throw new Error("Failed to fetch categories");
+   return response.json();
+}
+
+export async function createEstablishment(establishmentObject: Establishment) {
+   const response = await fetch("/api/v1/establishments", {
+      method: 'POST',
+      body: JSON.stringify(establishmentObject),
+   });
+
+   if (!response.ok) throw new Error("Failed to add a category!");
    return response.json();
 }
