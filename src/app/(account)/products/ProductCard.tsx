cb97659974cb994d3a84fc5ca5c4b8c7d8995605
AL <alcoran026@gmail.com>
2025-12-23 19:10:25 +0800

    [products] Change UI for product card information display

--- src/app/(account)/products/ProductCard.tsx
+++ src/app/(account)/products/ProductCard.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import { Item, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item";
 import { Product } from '@/lib/models';
-import { parseDecimalToLocalString } from '@/lib/helpers';
+import { parseDecimalToLocalString, priceDiscountCaculator, productHasDiscount } from '@/lib/helpers';
+import { Package } from 'lucide-react';
 
 export default function ProductCard({ 
    product 
@@ -22,15 +23,30 @@ export default function ProductCard({
                         priority
                         fill
                      />
+                     {productHasDiscount(product.discount_price) && (
+                        <span className="absolute text-xs top-0 right-0 bg-red-50 text-red-500 p-1 px-2">-{product.discount_price["$numberDecimal"]}%</span>
+                     )}
+                     
                   </div>
                   <div className='p-3'>
-                     <ItemTitle className="text-xs">{product.item_name}</ItemTitle>
-                     <ItemDescription className="text-xs line-clamp-1">
+                     <ItemTitle className="text-sm mb-4 line-clamp-2">{product.item_name}</ItemTitle>
+                     {/* <ItemDescription className="mb-3 text-xs line-clamp-1">
                         {product.description}
-                     </ItemDescription>
-                     <div className='mt-4 font-semibold'>₱ {parseDecimalToLocalString(product.price)}</div>
-                     <p className='mt-4 text-gray-500 text-xs line-clamp-1'>{product.establishment?.name}</p>
-                     <p className='text-gray-500 text-xs line-clamp-1'>{product.establishment?.address}</p>
+                     </ItemDescription> */}
+                     {productHasDiscount(product.discount_price) && (
+                        <div className='text-xs line-through text-gray-500'>₱ {parseDecimalToLocalString(product.price)}</div>
+                     )}
+                     
+                     <div className='font-semibold'>₱ {priceDiscountCaculator(product.price, product.discount_price)}</div>
+                     <div className='mt-4 text-xs flex items-center justify-between'>
+                        <div className='flex items-center gap-1'>
+                           <Package size={'13px'}/> 
+                           <span className='font-semibold'>{product.stock}</span>
+                        </div>
+                        <span>{product.stock} sold</span>
+                     </div>
+                     {/* <p className='mt-2 text-gray-500 text-xs line-clamp-1'>{product.establishment?.name}</p>
+                     <p className='text-gray-500 text-xs line-clamp-1'>{product.establishment?.address}</p> */}
                   </div>
                </ItemContent>
             </Item>
