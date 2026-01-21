f56dfea90bf749c3e442709d7ea11442c6f10e7b
AL <alcoran026@gmail.com>
2026-01-21 16:19:44 +0800

    [homepage] Added product listings component to the homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import AppFooter from "@/components/app-footer";
 import AppHomeCategories from "@/components/app-home-categories";
+import AppProductListings from "@/components/app-product-listings";
 
 export default function Home() {
    return (
@@ -7,6 +8,7 @@ export default function Home() {
          <div className="w-full mx-auto bg-gray-50 py-8">
             <div className="max-w-screen-xl mx-auto px-4 lg:px-6">
                <AppHomeCategories />
+               <AppProductListings/>
             </div>
          </div>
          <AppFooter />
