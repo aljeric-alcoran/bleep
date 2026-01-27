966554a098610a1c6343e3397f9ffeaf8bd6d27c
AL <alcoran026@gmail.com>
2026-01-27 16:51:22 +0800

    [homepage] Refactor layout: Transfer footer and content wrapper to the layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/app/assets/globals.css";
 import AppMainHeader from "@/components/app-main-header";
 import QueryClientProviders from "../providers/queryClientProvider";
+import AppFooter from "@/components/app-footer";
 
 export const metadata: Metadata = {
    title: "Bleep"
@@ -16,7 +17,12 @@ export default function RootLayout({
       <>
          <AppMainHeader/>
          <QueryClientProviders>
-            {children}
+            <div className="w-full mx-auto bg-gray-50 py-8">
+               <div className="max-w-screen-xl mx-auto px-4 lg:px-6">
+                  {children}
+               </div>
+            </div>
+            <AppFooter />
          </QueryClientProviders>
       </>
    );
