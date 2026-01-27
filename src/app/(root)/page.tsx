519bc34944de598624baab06863e3429b1d7e81b
AL <alcoran026@gmail.com>
2026-01-27 16:52:35 +0800

    [homepage] Transfer wrapper and footer to layout.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,17 +1,11 @@
-import AppFooter from "@/components/app-footer";
 import AppHomeCategories from "@/components/app-home-categories";
 import AppProductListings from "@/components/app-product-listings";
 
 export default function Home() {
    return (
       <>
-         <div className="w-full mx-auto bg-gray-50 py-8">
-            <div className="max-w-screen-xl mx-auto px-4 lg:px-6">
-               <AppHomeCategories />
-               <AppProductListings/>
-            </div>
-         </div>
-         <AppFooter />
+         <AppHomeCategories />
+         <AppProductListings/>
       </>
    );
 }
