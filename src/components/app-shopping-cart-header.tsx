24170f9fba122945bc35d328656c7eab11cfa74f
AL <alcoran026@gmail.com>
2026-01-31 18:01:02 +0800

    [cart] Add separate header for shopping Cart

---
+++ src/components/app-shopping-cart-header.tsx
@@ -0,0 +1,29 @@
+import AppHeader from "@/components/app-header";
+import Link from "next/link";
+import Image from "next/image";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+import SearchMain from "./header/search-main";
+import CartButton from "./header/cart-button";
+import BleepBrand from "./header/bleep-brand";
+
+export default function AppShoppingCartHeader() {
+   return (
+      <header className="border-b pb-6 bg-linear-to-t from-red-600 to-red-800 text-white">
+         <nav className="bg-transparent border-gray-200 px-4 lg:px-6 mx-auto max-w-screen-xl">
+            <div className="w-full flex flex-wrap justify-between items-center">
+
+               <AppHeader />
+
+               <div className="w-full max-w-7xl flex flex-wrap justify-between items-center py-5">
+                  <div className="w-full flex items-center justify-between gap-10">
+                     <BleepBrand optext="Shopping Cart"/>
+                     <SearchMain inputWidth="600px"/>
+                  </div>
+               </div>
+            </div>
+         </nav>
+      </header>
+   )
+}
