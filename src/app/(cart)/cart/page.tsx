de1e9b43dee21acef2a9fd2a09a903a0abeac562
AL <alcoran026@gmail.com>
2026-02-01 20:59:12 +0800

    [cart] Insert cart components

--- src/app/(cart)/cart/page.tsx
+++ src/app/(cart)/cart/page.tsx
@@ -1,9 +1,31 @@
+"use client"
+
+import CartHeader from "./CartHeaders";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCart } from "@/lib/api/cart";
+import { CartResponse } from "@/@types";
+import CartItem from "./CartItem";
+
 export default function ShoppingCart() {
+   const { isLoading, data, isError } = useQuery<CartResponse>({ 
+      queryKey:  ["cart"], 
+      queryFn: fetchCart,
+      retry: 2,
+   });
+
    return (
       <>
-         <div>
-            Shopping cart
-         </div>
+         <CartHeader/>
+
+         {isLoading ? (
+            <div>Loading...</div>
+         ) : (
+            <div className="mt-4 shadow w-full rounded-sm overflow-hidden">
+               {data?.items.map((item) => (
+                  <CartItem key={item.id} cartItem={item} />
+               ))}
+            </div>
+         )}
       </>
    );
 }
