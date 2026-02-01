cfbf94c8147b43a73b127835029e566718ee7fc0
AL <alcoran026@gmail.com>
2026-02-01 21:10:14 +0800

    [cart] Improve code quality

--- src/components/header/cart-button.tsx
+++ src/components/header/cart-button.tsx
@@ -2,22 +2,24 @@
 
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
-import { fetchCartItems } from "@/lib/api/cart";
+import { fetchCart } from "@/lib/api/cart";
 import { Badge } from "@/components/ui/badge";
 import { useQuery } from "@tanstack/react-query";
 
 export default function CartButton() {
-   const { isLoading, data } = useQuery({ queryKey:  ["cart"], queryFn: fetchCartItems});
-   let cartTotalItems = 0;
-
-   if (!isLoading) cartTotalItems = data.cart.total_items;
+   const {  data: cartTotalItems = 0, isLoading } = useQuery({ 
+      queryKey: ["cart"], 
+      queryFn: fetchCart,
+      select: (data) => data?.cart?.total_items ?? 0,
+      retry: 2,
+   });
 
    return (
       <div className="flex items-center gap-2">
          <div className="p-4 relative">
             <Link href="/cart">
                <ShoppingCart className="w-7 h-7 font-semibold" />
-               {!!cartTotalItems && (
+               {!isLoading && !!cartTotalItems && (
                   <Badge className="absolute top-0 right-0 bg-white text-red-800 font-semibold" variant="outline">{cartTotalItems}</Badge>
                )}
             </Link>
