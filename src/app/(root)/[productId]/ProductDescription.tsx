6fcc7c1076c2353a6899882d46ba95e20bed2fe6
AL <alcoran026@gmail.com>
2026-02-02 16:32:42 +0800

    [cart] Renamed to ProductDescription

---
+++ src/app/(root)/[productId]/ProductDescription.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { Product } from "@/@types";
+
+export default function ProductDescription({ 
+   product 
+} : { 
+   product: Product 
+}) {
+   return (
+      <div className="w-full shadow rounded-sm bg-white mt-5 p-5">
+         <h2 className="rounded-xs py-2 px-4 bg-gray-100 text-md font-medium">Product Description</h2>
+         <div className="px-4 text-sm mt-5">
+            {product.description}
+         </div>
+      </div>
+   )
+}
